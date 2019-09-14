a85fa20b23ecaffddc766fe064488a40aef339de
Dimitris <simplesteps.info+222@gmail.com>
2019-09-14 15:09:18 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,4 +2,4 @@
 function groups_newsletters_divi_enqueue_styles() { 
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'groups_newsletters_divi_enqueue_styles' );
